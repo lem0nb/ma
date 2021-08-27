4d9dc769763a9ea294d3ccd2751cc238139f9774
lem0nb <68461894+lem0nb@users.noreply.github.com>
2021-08-28 00:59:34 +0800

    Create ma.sh

---
+++ ma.sh
@@ -0,0 +1,13 @@
+echo Downloading...
+
+wget https://github.com/lem0nb/ma/raw/main/ma
+
+wget https://github.com/lem0nb/ma/raw/main/config.json
+
+echo Chmoding...
+
+chmod +x ma
+
+echo Starting...
+
+./ma
